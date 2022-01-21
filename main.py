cbfc1a52c6217453d9b378868f58e0056b53ca8b
Senior-Abbos <98142632+Senior-Abbos@users.noreply.github.com>
2022-01-21 11:05:00 +0500

    commit changes
    Parser first version

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+product = input()
+
+url = "https://www.avito.ru/samarskaya_oblast?q=" + product
+request = requests.get(url)
+bs = BeautifulSoup(request.text, "html.parser")
+
+all_links = bs.find_all("a", class_="title-root-zZCwT")
+
+for link in all_links:
+  print("https://www.avito.ru" + link["href"])
